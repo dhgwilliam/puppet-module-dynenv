29c1d7acdd62c1fdeb12cdf53ef315b189ecafc4
David Gwilliam <david@puppetlabs.com>
2012-12-05 18:27:18 -0700

    add confine to fact that uses /bin/cat

--- lib/facter/puppet-sync_pubkey.rb
+++ lib/facter/puppet-sync_pubkey.rb
@@ -1,4 +1,5 @@
 Facter.add("puppet_sync_pubkey") do
+  confine :kernel => "Linux"
   setcode do
     Facter::Util::Resolution.exec('cat /var/lib/puppet-sync/.ssh/id_rsa.pub | grep -o AAAA.*=')
   end
