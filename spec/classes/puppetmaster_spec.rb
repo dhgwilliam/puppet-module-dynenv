e63c9f67cfdc3b3bab76fee066b198979d2f70b1
David Gwilliam <david@puppetlabs.com>
2012-12-11 12:00:35 -0800

    rspec-puppet-init and first spec test

---
+++ spec/classes/puppetmaster_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'dynenv', :type => :class do
+  describe 'dynenv::init' do
+    let (:title) 
+    let (:params) { { 
+      :sync_user         => 'puppet-sync', 
+      :puppetmaster_fqdn => 'puppetmaster.puppetlabs.test',
+      :githost_fqdn      => 'githost.puppetlabs.test',
+      :git_repo          => '/var/lib/git/environments',
+    } }
+
+    context '$::fqdn matches puppetmaster_fqdn' do
+      let (:node) { 'puppetmaster.puppetlabs.test' }
+
+      it { should include_class('dynenv::puppetmaster') }
+    end
+
+  end
+end
