c3d169d53d59f9c789197341cad8cf2ef3967c16
David Gwilliam <david@puppetlabs.com>
2012-12-13 14:12:38 -0800

    (#10) check for presence of private key before cat-ing it

--- lib/facter/puppet-sync_pubkey.rb
+++ lib/facter/puppet-sync_pubkey.rb
@@ -1,7 +1,14 @@
 Facter.add("puppet_sync_pubkey") do
   confine :kernel => "Linux"
   setcode do
-    Facter::Util::Resolution.exec('cat /var/lib/puppet-sync/.ssh/id_rsa.pub | grep -o AAAA.*=')
+    Facter::Util::Resolution.exec(
+      '
+      if [ -e "/var/lib/puppet-sync/.ssh/id_rsa.pub" ]
+      then
+        cat /var/lib/puppet-sync/.ssh/id_rsa.pub | grep -o AAAA.*=
+      fi
+      '
+    )
   end
 end
 
